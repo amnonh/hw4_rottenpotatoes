4f2c767afc67dc6c7f0a832fb4b4ec35013aa612
saasbook <saasbook@saasbook.(none)>
2012-08-14 14:22:48 -0700

    cucumber test passed

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,17 +3,20 @@ require 'spec_helper'
 describe MoviesController do
   describe 'searching movies by the same director' do
     it 'should call the model method that performs search by director' do
-      Movie.should_receive(:search_by_director).with('1')
+      fake_results = [mock('Movie'), mock('Movie')]
+      Movie.should_receive(:search_by_director).with('1').and_return(fake_results)
       get :search_by_director, :id => 1
     end
     it 'should select the Search Results template for rendering' do
-      Movie.stub(:search_by_director)
+      fake_results = [mock('Movie'), mock('Movie')]
+      Movie.stub(:search_by_director).and_return(fake_results)
       get :search_by_director, :id => 1
       response.should render_template('search_by_director')
     end
     it 'should make the search results available to that template' do
       fake_results = [mock('Movie'), mock('Movie')]
       Movie.stub(:search_by_director).and_return(fake_results)
+      Movie.stub(:find)
       get :search_by_director, :id => 1
       # look for controller method to assign @movies
       assigns(:movies).should == fake_results
